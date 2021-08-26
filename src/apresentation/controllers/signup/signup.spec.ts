4e1bb63a811016ffc10dd4e6b3d1630ce214856e
Alquipo <alquiponeto@outlook.com.br>
2021-08-25 21:16:34 -0300

    test: ensure SignUpControlle return 500 if AddAccount throw error

--- src/apresentation/controllers/signup/signup.spec.ts
+++ src/apresentation/controllers/signup/signup.spec.ts
@@ -173,6 +173,25 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new ServerError())
   })
 
+  test('should return 500 if AddAccount throws', () => {
+    const { sut, addAccountStub } = makeSut()
+    jest.spyOn(addAccountStub, 'add').mockImplementation(() => {
+      throw new Error()
+    })
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
   test('should call AddAccount with correct values', () => {
     const { sut, addAccountStub } = makeSut()
     const addSpy = jest.spyOn(addAccountStub, 'add')
