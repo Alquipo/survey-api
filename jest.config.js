865cad288372a7fbb1079cafc181f267eaf52230
Alquipo <alquiponeto@outlook.com.br>
2021-08-25 22:06:51 -0300

    chore: add jest configs and scripts

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-
-  // The directory where Jest should output its coverage files
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
   coverageDirectory: 'coverage',
-
-  // A map from regular expressions to paths to transformers
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
